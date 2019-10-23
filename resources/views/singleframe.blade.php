ba7fab3270161cf28e75ff08f7f3f556d12f95bf
MAQ-97 <asad.muhammad.qazi@gmail.com>
2019-10-23 22:33:09 +0500

    image size increase

--- resources/views/singleframe.blade.php
+++ resources/views/singleframe.blade.php
@@ -22,7 +22,7 @@
         {{-- <div class=""> --}}
             <div class="row">
                 <div class="col-md-12">
-                    <div class="image-frames"><img src="http://testapi.framesdata.com/api/images?auth=4046ecc3-bc75-4bdf-ba48-e11ef8c8a0c1&size=Small&id=116807"></div>
+                    <div class="image-frames"><img src="http://testapi.framesdata.com/api/images?auth=4046ecc3-bc75-4bdf-ba48-e11ef8c8a0c1&size=Large&id=116807"></div>
                 </div>
             </div>
             <div class="row">
